0e92b40027453773bca4162a387a7639bc6b4470
IlSanna <federico.sannasardo@outlook.com>
2019-09-21 14:04:28 +0200

    little refactoring with 2 new void functions to clear the cose, and also changed the names of audio variavles to be more clear

--- Pong/Main.cpp
+++ Pong/Main.cpp
@@ -4,6 +4,34 @@
 #include <SFML\Audio.hpp>
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")//per evitare il pop up della console
 
+void GameOver(sf::RenderWindow &window, sf::Text &text, int windowsWidth,int windowsHeight,int score,int lives) {
+	text.setPosition((windowsWidth / 2) - 100, windowsHeight / 2 - 20);
+	text.setString("YOU LOOSE\n\ny continue...");
+	window.clear(sf::Color::Blue);
+	window.draw(text);//pensare ad una function che loop throug an array and draw each element
+	window.display();
+	while (!sf::Keyboard::isKeyPressed(sf::Keyboard::Y)) {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+			window.close();
+			return;
+		}
+	};
+	text.setPosition(0, 0);
+	score = 0; lives = 3;
+}
+void SetupAudio(sf::SoundBuffer &normalBounceBuffer, sf::SoundBuffer &batBounceBuffer, sf::SoundBuffer &outBuffer,
+	sf::Sound &normalBounceSound, sf::Sound &outSound, sf::Sound &batBounceSound) {
+	normalBounceBuffer.loadFromFile("Audio/bounceNormal.ogg");
+	batBounceBuffer.loadFromFile("Audio/bounce.ogg");
+	outBuffer.loadFromFile("Audio/out.ogg");
+
+	normalBounceSound.setBuffer(normalBounceBuffer);
+	normalBounceSound.setVolume(50);
+	outSound.setBuffer(outBuffer);
+	outSound.setVolume(30);
+	batBounceSound.setBuffer(batBounceBuffer);
+}
+
 int main() {
 	//create a window
 	int windowsWidth = 512;
@@ -21,25 +49,18 @@ int main() {
 	text.setFont(font);
 	text.setCharacterSize(28);
 	text.setFillColor(sf::Color::White);
-
+	//initial player values
 	int score = 0;
 	int lives = 3;
 
-	//dealing with the audio
-	sf::SoundBuffer buffer1;
-	buffer1.loadFromFile("Audio/bounceNormal.ogg");
-	sf::SoundBuffer buffer2;
-	buffer2.loadFromFile("Audio/bounce.ogg");
-	sf::SoundBuffer buffer3;
-	buffer3.loadFromFile("Audio/out.ogg");
-	sf::Sound normalBounce;
-	normalBounce.setBuffer(buffer1);
-	normalBounce.setVolume(50);
-	sf::Sound out;
-	out.setBuffer(buffer3);
-	out.setVolume(30);
-	sf::Sound bounce;
-	bounce.setBuffer(buffer2);
+	//DEALING WITH AUDIO
+	sf::SoundBuffer normalBounceBuffer;
+	sf::SoundBuffer batBounceBuffer;
+	sf::SoundBuffer outBuffer;
+	sf::Sound normalBounceSound;
+	sf::Sound batBounceSound;
+	sf::Sound outSound;
+	SetupAudio(normalBounceBuffer,batBounceBuffer,outBuffer,normalBounceSound,batBounceSound,outSound);
 
 	//main loop
 	while (window.isOpen()) {
@@ -66,39 +87,27 @@ int main() {
 		//if the top position of the ball exceeds the windowsheight->the ball is going out
 		if (ball.getPosition().top > windowsHeight) {
 			ball.hitBottom();
-			out.play();
+			outSound.play();
 			lives--;
 			if (lives < 1) {//player loose,reset the game
-				text.setPosition((windowsWidth / 2) - 100, windowsHeight/2-20);
-				text.setString("YOU LOOSE\n\ny continue...");
-				window.clear(sf::Color::Blue);
-				window.draw(text);//pensare ad una function che loop throug an array and draw each element
-				window.display();
-				while (!sf::Keyboard::isKeyPressed(sf::Keyboard::Y)) {
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-						window.close();
-						return 0;
-					}
-				};
-				text.setPosition(0, 0);
-				score = 0; lives = 3;
+				GameOver(window, text, windowsWidth, windowsHeight,score,lives);
 			}
 		}
 		//if the ball hit the top of the screen
 		if (ball.getPosition().top < 0) {
 			ball.reboundBatOrTop();
-			normalBounce.play();
+			normalBounceSound.play();
 			score++;
 		}
 		//if the ball hits the sides of the screen, 10 is the ball size in ball.cpp
 		if (ball.getPosition().left < 0 || ball.getPosition().left + 10> windowsWidth) {
 			ball.reboundSides();
-			normalBounce.play();
+			normalBounceSound.play();
 		}
 		//if the ball intersects the bat
 		if (ball.getPosition().intersects(bat.getPosition())) {
 			ball.reboundBatOrTop();
-			bounce.play();
+			batBounceSound.play();
 		}
 		//if the bat reach the sides of the screen
 		if (bat.getPosition().left < 0 ) {
@@ -115,7 +124,8 @@ int main() {
 		ss << "Score: " << score << "    Lives: " << lives;
 		text.setString(ss.str());
 
-		//LET'S HANDLE THE DRAWINGS
+		//LET'S HANDLE THE DRAWINGS 
+		//TODO set the draw to be a loop
 		window.clear(sf::Color::Blue);//clear the previous frame
 		window.draw(bat.getShape());//draw bat
 		window.draw(ball.getShape());//draw ball
@@ -125,3 +135,4 @@ int main() {
 
 	return 0;
 }
+
