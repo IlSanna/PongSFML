578d2194cb73dbdd2d2efa97fa4049a0d3ae796b
IlSanna <IlSanna@users.noreply.github.com>
2019-09-24 16:24:36 +0200

    Let's Start

---
+++ Pong/Game.h
@@ -0,0 +1,16 @@
+#pragma once
+class Game {
+public:
+	Game() {}
+	~Game() {}
+	// simply set the running variable to true
+	void init() { m_bRunning = true; }
+	void render() {}
+	void update() {}
+	void handleEvents() {}
+	void clean() {}
+	// a function to access the private running variable
+	bool running() { return m_bRunning; }
+private:
+	bool m_bRunning;
+};
